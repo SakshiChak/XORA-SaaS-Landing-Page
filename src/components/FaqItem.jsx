410aebbfa0ff11884e9d571c6b50f2adcb894e62
SakshiChak <mailme.sakshichak@gmail.com>
2024-10-08 17:59:55 +0530

    update faq section

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -5,6 +5,7 @@ import "react-slidedown/lib/slidedown.css";
 
 const FaqItem = ({ item, index }) => {
 	const [activeId, setActiveId] = useState(null);
+
 	const active = activeId === item.id;
 
 	return (
@@ -17,8 +18,8 @@ const FaqItem = ({ item, index }) => {
 			>
 				<div className="flex-1">
 					<div className="small-compact mb-1.5 text-p3 max-lg:hidden">
-						{index < 10 ? "0" : ""}
-						{index}
+						{index < 9 ? "0" : ""}
+						{index + 1}
 					</div>
 					<div
 						className={clsx(
@@ -36,7 +37,7 @@ const FaqItem = ({ item, index }) => {
 						active && "before:bg-p1 after:rotate-0 after:bg-p1"
 					)}
 				>
-					<div className="g4 size-11/12 rounded-full shadow-300"></div>
+					<div className="g4 size-11/12 rounded-full shadow-300" />
 				</div>
 			</div>
 
@@ -47,7 +48,10 @@ const FaqItem = ({ item, index }) => {
 			</SlideDown>
 
 			<div
-				className={clsx("g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 absolute", active && "opacity-100")}
+				className={clsx(
+					"g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 absolute",
+					active && "opacity-100"
+				)}
 			>
 				<div className="g4 absolute inset-0.5 -z-1 rounded-3xl" />
 				<div className="absolute left-8 top-0 h-0.5 w-40 bg-p1" />
@@ -55,5 +59,4 @@ const FaqItem = ({ item, index }) => {
 		</div>
 	);
 };
-
 export default FaqItem;
